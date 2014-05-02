2a60acabe697cf255ddf74eeb11fcee933014f33
Matthew Swartwout <mws85@case.edu>
2014-05-01 21:11:55 -0400

    Code Clean-Up
    Cleans up code, adds more comments, removes dead code. Still does not
    function, except with certain values. Haven't been able to ascertain
    what causes it to not work with some numbers, but there is an error
    where the working value becomes NaN. Using float point numbers is not
    necessary, removing the use of floating points could fix errors.

--- EncryptRead.asm
+++ EncryptRead.asm
@@ -1,50 +1,38 @@
+#Data section contains prompts, file location, and input and output spaces
 .data
-promptED: .asciiz "\nWhat do you want to do with the file?\n0: Encryption\n1:Decryption\n... "
-promptE: .asciiz "Enter the encryption key.\n"
-promptM: .asciiz "Enter the modulus.\n"
-promptExit: .asciiz "\nWould you like to exit?\n0: Decrypt/Encrypt another file.\n1: Exit.\n..."
-promptFN:	.asciiz	"Enter the file name you wish to input: "
-fname:	.asciiz	"C:\\Users\\Public\\rsa.txt"	# file name to read
-fout:   .asciiz "C:\\Users\\Public\\rsa.txt"      # filename for output
-promptC: .asciiz "Enter a shift number: "
-output: .space 15
-input:	.space 15 # location for input from file
-#actual start of the main program
+promptED: 	.asciiz 	"\nWhat do you want to do with the file?\n0: Encryption\n1:Decryption\n... "
+promptE: 	.asciiz 	"Enter the encryption key.\n"
+promptM: 	.asciiz 	"Enter the modulus. The modulus cannot be greater than 255\n"
+promptExit:	.asciiz 	"\nWould you like to exit?\n0: Decrypt/Encrypt another file.\n1: Exit.\n..."
+promptFN:	.asciiz		"Enter the file name you wish to input: "
+fname:		.asciiz		"C:\\Users\\Public\\rsa.txt"	# file name to read
+fout:   	.asciiz 	"C:\\Users\\Public\\rsa.txt"	# filename for output
+promptC: 	.asciiz 	"Enter a shift number: "
+output: 	.space 		15	#location for output to file
+input:		.space 		15	# location for input from file
+
+#Begin code
 .text
 .globl main
-main:				#main has to be a global label
-
-	#RSA numbers: Modulus = 143, encryption key = 7, decryption key = 103
+main:				
+	#Modulus can be no greater than 255
+	#Suggested RSA numbers: [Modulus = 143, encryption key = 7, decryption key = 103], [255,3,43], [255,7,55], [187,7,23], [153,7,55], [147,5,17]
 	jal encryptOrDecrypt
-	#beqz $s0, encrypt
-	#jal getFileName	#Not working right now
 	jal getModulus
 	jal getExponent
 	jal readFile
-	#jal testPrintInput
 	jal rsaMath
-	#jal testPrintOutput
-
-	#jal shiftCypherD
 	jal writeFile
 	jal exit		
-	#beq	$s0, $0, Encrypt
-	#j Decrypt
-
-#encrypt:
-#	jal readFile
-#	jal shiftCypherE
-#	jal writeFile
-#	jal exit
 
 encryptOrDecrypt:
 	la	$a0, promptED	#prompt for encrypt or decrypt
-	li	$v0,4
+	li	$v0,4		#syscall value for print string is 4
 	syscall
 	
-	li	$v0, 5
+	li	$v0, 5		#syscall value for read integer is 5
 	syscall
-	add $s0, $v0, $zero	#save user input to register s0(0:e, 1:d)
+	add $s0, $v0, $zero	#save user input to register s0 (0=encrypt, 1=decrypt)
 	
 	jr $ra
 
@@ -60,9 +48,10 @@ encryptOrDecrypt:
 #	syscall
 #
 #	jr	$ra		# return to main method
-		
+
+#Prompts the user to enter the modulus
 getModulus:
-	la $a0, promptM
+	la $a0, promptM		#Loads the prompt to enter the modulus
 	li $v0, 4
 	syscall
 
@@ -71,18 +60,20 @@ getModulus:
 	mov.d $f2, $f0	#modulus stored in f0 is moved to f2
 	
 	jr $ra
-	
+
+#Prompts the user to enter the exponent
 getExponent:
-	la $a0, promptE
+	la $a0, promptE		#Loads the prompt to enter the exponent
 	li $v0, 4
 	syscall
 
 	li $v0, 5
 	syscall
-	add $s2, $v0, $zero	#encryption key in $s7 ($s7 = e)
+	add $s2, $v0, $zero	#exponent is stored in s2
 
 	jr $ra	
 
+#Reads the contents of the file into memory
 readFile:	
 	li   	$v0, 13       	# system call for open file
  	la   	$a0, fname   	# output file name
@@ -90,77 +81,33 @@ readFile:
   	li   	$a2, 0       	# mode is ignored
   	syscall            	# open a file (file descriptor returned in $v0)
 
-  	add 	$s6, $v0, $zero     	# save the file descriptor 
+  	add 	$s6, $v0, $zero	# save the file descriptor 
   
  	# read from file just opened
  	li   	$v0, 14      	# system call for read from file
-  	add 	$a0, $s6, $zero     	# file descriptor 
+  	add 	$a0, $s6, $zero # file descriptor 
   	la   	$a1, input  	# address to store text from file
-  	li   	$a2, 15      # hardcoded buffer length
+  	li   	$a2, 15      	# hardcoded buffer length
   	syscall           	# read file
   	
  	# Close the file 
   	li   	$v0, 16      	# system call for close file
-  	add 	$a0, $s6, $zero     	# file descriptor to close
+  	add 	$a0, $s6, $zero # file descriptor to close
   	syscall          	# close file
 
-	#jal	Test_Print	#NEED TO SAVE ADDRESS TO DIFFERENT REGISTER!!!
-	#jal	Test_Change	#Test functions commented out
-
 	jr $ra
-
-#testPrintInput:
-#	la	$a0, input
-#	li	$v0, 4
-#	syscall
-#
-#	jr $ra
 	
-#testPrintOutput:
-#	la	$a0, output
-#	li	$v0, 4
-#	syscall
-#	
-#	jr $ra
-
-#testPrintChar:
-#	la	$a0, ($t4)
-#	li	$v0, 4
-#	syscall
-#	
-#	jr $ra
-
-#testChange:
-#	li	$v0, 4
-#	la	$t0, input
-	
-#printLoop:
-#	lb	$t2, ($t0)
-#	beqz	$t2, JReturn
-#	#jal	encrChar
-#	sb	$t2, ($t0)
-#	addi	$t0, $t0, 1
-#	j	printLoop
-
-#JReturn:
-#	la	$a0, input
-#	syscall
-#	
-#	jr	$ra
-	
+#Begins the RSA character manipulation math
 rsaMath:
-	move $s3, $ra 	#saves the return address into s3 because a jal is called later in powerE
-	la $t0, input	#puts address of input into $t0
-	la $t4, output #puts address of output into $t4
-	addi $t0, $t0, -1 #decrements t0 so that the first loop of iterateCharacters does not skip the first character in the file
-	addi $t4, $t4, -1 #decrements t4 so that the first loop of iterateCharacter writes the initial garbage to a wrong location
-#	addi $t8, $t8, 32767
-#	mtc1.d $t8, $f4
-#	cvt.d.w $f4, $f4
-	
+	move $s3, $ra		#saves the return address into s3 because a jal is called later in powerE
+	la $t0, input		#puts address of input into $t0
+	la $t4, output 		#puts address of output into $t4
+	addi $t0, $t0, -1 	#decrements t0 so that the first loop of iterateCharacters does not skip the first character in the file
+	addi $t4, $t4, -1 	#decrements t4 so that the first loop of iterateCharacter writes the initial garbage to a wrong location
+
+#Loop that iterates through each character
 iterateCharacters:
 	move $t1, $s2		#copies exponent into $t1
-	#jal testPrintChar
 	cvt.w.d $f6, $f6 	#converts the working value (f6) from a double-precision floating point to an integer
 	mfc1 $t2, $f6 		#moves the new integer value in f6 to t2
 	sb $t2, ($t4) 		#stores the value of t2 in t4
@@ -172,7 +119,8 @@ iterateCharacters:
 	
 	mov.d $f8, $f6 		#makes a copy of the original char, for use in powerE
 	beqz $t2, exitRsaMath 	#if the most recent char that has been read is zero, then the EOF has been reached, and exits encryption
-	
+
+#Loop that iterates through taking each character to the proper power	
 powerE:	
 	beq $t1, 1, modN	#if the power remaining = 1, take the mod
 	mul.d $f6, $f6, $f8	#multiply current char by original char
@@ -181,112 +129,55 @@ powerE:
 	bgt $t1, 1, powerE	#branch to top of loop if not fininshed
 	j iterateCharacters
 
+#Takes the modulus of the working value in f6
 modN:
 	div.d $f6, $f6, $f2	#divide the working value (f6) by the modulus (f2)
-	trunc.w.d $f10, $f6	#truncate the working value (f6) and store in f10 (2.xxxxx -> 2)
+	floor.w.d $f10, $f6	#truncate the working value (f6) and store in f10 (2.xxxxx -> 2)
 	cvt.d.w $f10, $f10	#converts the truncated floating point value to an integer
 	sub.d $f6, $f6, $f10	#subtracts working value (f6) - truncated value (f10) and stores in f6 (2.xxxxx - 2 = .xxxxx)
 	mul.d $f6, $f6, $f2	#sets working value equal to remainder of division by multiplying decimal value (f6) by the modulus (f2)
 	round.w.d $f6, $f6	#rounds the working value in case there is a slight error in the division/multiplication
 	cvt.d.w $f6, $f6	#converts the integer now in f6 to a double precision floating point
-#	c.lt.d $f6, $f4
-#	bc1t modI
-#	sub.d $f6, $f6, $f2
-#	j modN
-	#slt $t3, $t2, $s1	#if working value < n, t3 = 1
-	#bne $t3, $0, iterateCharacters		#exit loop (loop performs mod n)
-	#sub $t2, $t2, $s1	#subtract n from the working value
+
 	jr $ra
 
-#modI:
-#	cvt.w.d $f6, $f6
-#	mfc1 $t9, $f6
-#	cvt.w.d $f2, $f2
-#	mfc1 $s7, $f2
-#	div $t9, $s7
-#	mfhi $t2
-#	j iterateCharacters
-
+#Exits the rsaMath loop 
 exitRsaMath:
 	jr $s3
 	
-writeFile: # Create and open a new .txt file
-	li   $v0, 13       # system call for open file
-	la   $a0, fout     # address of output file name
-	li   $a1, 1        # Open for writing flags 1 for write
-	li   $a2, 0        # mode is ignored
-	syscall            # open a file (file descriptor returned in $v0)
-	add $s6, $v0, $zero      # save the file descriptor 
+#Writes the new text to the file
+writeFile:
+	li   $v0, 13       	# system call for open file
+	la   $a0, fout     	# address of output file name
+	li   $a1, 1        	# Open for writing flags 1 for write
+	li   $a2, 0        	# mode is ignored
+	syscall            	# open a file (file descriptor returned in $v0)
+	add $s6, $v0, $zero     # save the file descriptor 
 
 	# Write to file just opened
-	li   $v0, 15       # system call for write to file
-	move $a0, $s6      # file descriptor 
-	la   $a1, output   # address of buffer from which to write
-	li   $a2, 15     # hardcoded buffer length=1000
-	syscall            # write to file
+	li   $v0, 15       	# system call for write to file
+	move $a0, $s6      	# file descriptor 
+	la   $a1, output   	# address of buffer from which to write
+	li   $a2, 15     	# hardcoded buffer length=1000
+	syscall            	# write to file
 
 	# Close the file 
-	li   $v0, 16       # system call for close file
-	add $a0, $s6, $zero      # file descriptor to close
-	syscall            # close file
+	li   $v0, 16       	# system call for close file
+	add $a0, $s6, $zero     # file descriptor to close
+	syscall            	# close file
 
 	jr $ra
 
-#shiftCypherE:
-#	la $a0, promptC
-#	li $v0, 4
-#	syscall
-#
-#	li $v0, 5
-#	syscall
-#	add $s2, $v0, $zero	
-#	
-#	la $t0, input	#puts address of input into $t0
-#	la $t4, output #puts address of output into $t4
-
-#shiftE:
-#	lb $t2, ($t0)
-#	beqz $t2, rtm
-#	add $t2, $t2, $s2
-#	sb $t2, ($t4)
-#	addi $t0, $t0, 1
-#	addi $t4, $t4, 1
-#	j shiftE
-
-#shiftCypherD:
-#	la $a0, promptC
-#	li $v0, 4
-#	syscall
-#
-#	li $v0, 5
-#	syscall
-#	add $s2, $v0, $zero	
-#	
-#	la $t0, input	#puts address of input into $t0
-#	la $t4, output #puts address of output into $t4
-
-#shiftD:
-#	lb $t2, ($t0)
-#	beqz $t2, rtm
-#	sub $t2, $t2, $s2
-#	sb $t2, ($t4)
-#	addi $t0, $t0, 1
-#	addi $t4, $t4, 1
-#	j shiftD
-
-#rtm:
-#	jr $ra
-	
+#User decides whether to terminate the program, or restart
 exit:	
-	la $a0, promptExit
+	la $a0, promptExit	#Loads exit prompt
 	li $v0, 4
 	syscall
 
 	li $v0, 5
 	syscall
-	add $s0, $v0, $0
 
-	beq $v0, $0, main
+	beq $v0, $0, main	#If user enters 0, restarts program
 
-	li $v0, 10
+	li $v0, 10		#else quit
 	syscall
